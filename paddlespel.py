5d754ffb779277755423824a9cf095a089543b58
Kasper Reynders <kasper.reynders@gmail.com>
2021-12-24 11:02:22 +0100

    paddle

--- paddlespel.py
+++ paddlespel.py
@@ -3,8 +3,9 @@ import random
 import time
 
 class Bal:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         starts = [-3, -2, -1, 1, 2, 3]
@@ -13,6 +14,14 @@ class Bal:
         self.y = -3
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return  True
+        return False
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -20,11 +29,37 @@ class Bal:
         if pos[1] <= 0:
             self.y = 3
         if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if hit_paddle == True:
             self.y = -3
         if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
+
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def turn_left(self, evt):
+        self.x = -2
+
+    def turn_right(self, evt):
+        self.x = 2
 
 tk = Tk()
 tk.title("spel")
@@ -34,10 +69,13 @@ canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
-bal = Bal(canvas, 'red')
+paddle = Paddle(canvas, 'blue')
+bal = Bal(canvas, paddle, 'red')
 
 while 1:
-    bal.draw()
+    if bal.hit_bottom == False:
+        paddle.draw()
+        bal.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
